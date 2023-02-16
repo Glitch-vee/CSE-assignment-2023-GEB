9685f4042d0d5c895e5d6d9611d4130b113209dc
Glitch-vee <120122005+Glitch-vee@users.noreply.github.com>
2023-02-16 13:01:36 +0600

    Update Q-9.py

--- Q-9.py
+++ Q-9.py
@@ -1,21 +1,4 @@
-def is_prime(n):
-    if n < 2:
-        return False
-    for i in range(2, int(n**(1/2))+1):
-        if n % i == 0:
-            return False
-    return True
-
-def main():
-    number = int(input("Enter a positive integer: "))
-    if number > 0:
-        result = is_prime(number)
-        if result:
-            print(number, "is a prime number.")
-        else:
-            print(number, "is not a prime number.")
-    else:
-        print("Invalid input. Please enter a positive integer.")
-
-if __name__ == "__main__":
-    main()
+#9
+user_input = input()
+new_string = user_input[-1] + user_input[1:-1] + user_input[0]
+print(new_string)
