db104291d2c41b26c651646f19f854622f4c0679
Glitch-vee <120122005+Glitch-vee@users.noreply.github.com>
2023-02-16 13:00:42 +0600

    Update Q-8.py

--- Q-8.py
+++ Q-8.py
@@ -1,16 +1,20 @@
-class SumOfDigits:
-    def __init__(self, number):
-        self.number = number
-
-    def sum_of_digits(self):
-        digit_list = [int(d) for d in str(self.number)]
-        return sum(digit_list)
+#8
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**(1/2))+1):
+        if n % i == 0:
+            return False
+    return True
 
 def main():
-    number = int(input("Enter a positive integer: "))
+    number = int(input())
     if number > 0:
-        obj = SumOfDigits(number)
-        print("Sum of digits of", number, "is", obj.sum_of_digits())
+        result = is_prime(number)
+        if result:
+            print("Prime")
+        else:
+            print("Not Prime")
     else:
         print("Invalid input. Please enter a positive integer.")
 
