50d9ceeb29c7f5238836aa7cb4f9190ea80a86e0
Glitch-vee <120122005+Glitch-vee@users.noreply.github.com>
2023-02-16 12:57:22 +0600

    Update Q-6.py

--- Q-6.py
+++ Q-6.py
@@ -1,7 +1,6 @@
-n = int(input("Enter the number of row= "))
-p = 1
-for i in range(1, n+1):
-    for j in range(i):
-        print(p, end=' ')
-        p += 1
-    print('')
+#6
+r = int(input())
+x=0
+for i in range(1,r+1):
+    x+=i
+print(x)   
