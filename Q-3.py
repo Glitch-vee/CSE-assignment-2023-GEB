e87d1fb0dc02633db93b92ca640ecf07a31d97ae
Glitch-vee <120122005+Glitch-vee@users.noreply.github.com>
2023-02-16 12:55:03 +0600

    Update Q-3.py

--- Q-3.py
+++ Q-3.py
@@ -1,24 +1,11 @@
-#Write a program that prompts the user to input two numbers and display its HCF. The Highest Common Factor (HCF) also called the Greatest Common Divisor (GCD) of two whole numbers, is the largest whole number that’s a factor of both of them
-
-num1, num2 =input("enter first number= "),input("enter second number= ")
-if (num1.isdigit()== True and num2.isdigit()==True):
-  def get_hcf(x,y):
-      if x>y:
-          min = y
-      else:
-          min = x
-      for i in range(min,1, -1):
-          if x%i == 0 and y%i == 0:
-              return i
-      return 1
-  
-  print("The HCF of %d and %d is %d" % (int(num1), int(num2), get_hcf(int(num1), int(num2))))
-           
-else:
-  print("Please enter two whole numbers")
-    
-  
-  
-  
-  
-
+#3
+n = int(input())      
+for i in range(n):
+          for j in range(i, n):
+              print("", end=" ")
+          for j in range(i):
+              
+              print(i+1,end="") 
+          for j in range(i+1):
+              print(i+1,end="")               
+          print()
