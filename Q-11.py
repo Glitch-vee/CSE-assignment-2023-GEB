00d5dd7bd8996ecd2a1549f24261c25f1749f305
Glitch-vee <120122005+Glitch-vee@users.noreply.github.com>
2023-02-16 13:05:15 +0600

    Update Q-11.py

--- Q-11.py
+++ Q-11.py
@@ -1,6 +1,4 @@
-user_input = input("Please enter a string: ")
-old_letter = input("Please enter the letter you want to replace: ")
-new_letter = input("Please enter the letter you want to replace with: ")
-
-new_string = user_input.replace(old_letter, new_letter)
-print("The new string is:", new_string)
+#11
+line1 =input("Enter a a word and two letters: ").split()
+word = line1[0].replace(line1[1],line1[2])
+print(word)
