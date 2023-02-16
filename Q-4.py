c69cc4045ba94f9996370dffbf8f699006bb3989
Glitch-vee <120122005+Glitch-vee@users.noreply.github.com>
2023-02-16 12:56:14 +0600

    Update Q-4.py

--- Q-4.py
+++ Q-4.py
@@ -1,11 +1,14 @@
-#number triangle
-n = int(input("Enter the number of rows: "))      
+#4
+n = int(input())      
 for i in range(n):
+          p=1
           for j in range(i, n):
-              print(" ", end=" ")
-          for j in range(i):
-              
-              print(i+1,end=" ") 
+              print("", end=" ")
+             
+          for j in range(i,0,-1):       
+              print(j+1,end="") 
+           
           for j in range(i+1):
-              print(i+1,end=" ")               
+              print(p,end="")        
+              p+=1       
           print()
