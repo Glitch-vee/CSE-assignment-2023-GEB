0151da1efbe513cc080562d4d97ecf4b738e31f8
Glitch-vee <120122005+Glitch-vee@users.noreply.github.com>
2023-02-16 12:53:19 +0600

    Update Q-2.py

--- Q-2.py
+++ Q-2.py
@@ -1,7 +1,21 @@
-#1. Write a program that takes an integer value input and print its binary, octal and hexadecimal value as output
-
-try:
-    x=int(input("Enter number= "))
-    print(bin(x),oct(x),hex(x), sep='\n')
-except:
-    print("thats not an integer")
+#2
+nums = input("Enter two number: ").split()
+num1= (nums[0])
+num2= (nums[1])
+if (num1.isdigit()== True and num2.isdigit()==True):
+  def get_hcf(x,y):
+      if x>y:
+          min = y
+      else:
+          min = x
+      for i in range(min,1, -1):
+          if x%i == 0 and y%i == 0:
+              return i
+      return 1
+  
+  print( get_hcf(int(num1), int(num2)))
+           
+else:
+  print("Please enter two whole numbers")
+    
+  
