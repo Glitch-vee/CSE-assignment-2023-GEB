186725069171811596fa501116bd2474fb5184b0
Glitch-vee <120122005+Glitch-vee@users.noreply.github.com>
2023-02-16 12:36:56 +0600

    Update Q-1.py

--- Q-1.py
+++ Q-1.py
@@ -1,6 +1,6 @@
-#1 
+#1
 try:
- x=int(input("Enter number= ")) 
- print(bin(x)[2:],oct(x)[2:],hex(x)[2:], sep='\n')
-except: 
- print("thats not an integer")
+    x=int(input("Enter number= "))
+    print(bin(x)[2:],oct(x)[2:],hex(x)[2:], sep='\n')
+except:
+    print("thats not an integer")
